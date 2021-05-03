61cb6f921960291ea65e4eb92992386198fa2794
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-05-03 10:02:00 +0200

    test: Add tests for containsProps function.

--- vscode-convert-css/src/test/suite/helperFunction.test.ts
+++ vscode-convert-css/src/test/suite/helperFunction.test.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import {
+  containsProps,
   getProps,
   isUnitlessCssProperty,
   removeDuplicates,
@@ -24,6 +25,61 @@ suite("Extension Utils Tests for removeDuplicates function", function () {
     const deduplicatedArr = removeDuplicates(duplicates);
 
     assert.deepStrictEqual(withoutDuplicates, deduplicatedArr);
+  });
+});
+
+suite("Extension Utils Tests for containsProps function", function () {
+  test("Should return true when (not destructured) props are present in styled component with style object syntax", function () {
+    const code =
+      'const PropsExample = styled.div((props) => ({\n  [`${props.vertical ? "margin-top" : "margin-left"}`]: "1rem",\n  [`${props.vertical && "margin-top"}`]: "10px",\n  [`${props.primary && "color"}`]: "blue",\n  fontSize: "12px",\n  zIndex: 0,\n}));';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return true when (not destructured) props are present in styled component css property using css backticks syntax", function () {
+    const code =
+      'const PropsExample = styled.div`\n  ${(props) => props.flex || "display: block"};\n  ${(props) => props.vertical && "flex-direction: column;"};\n`;';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return true when (not destructured) props are present in styled component containing css value using css backticks syntax", function () {
+    const code =
+      'const PropsExample = styled.div`\n  color: ${(props) => (props.green ? "green" : "purple")};\n`;';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return true when (not destructured) props are present in css property using css backticks syntax", function () {
+    const code = '${(props) => props.vertical && "flex-direction: column;"};';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return true when (not destructured) props are present in css value using css backticks syntax", function () {
+    const code = 'color: ${(props) => (props.green ? "green" : "purple")};';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return true when (not destructured) props are present in css property using css style object syntax", function () {
+    const code = '[`${props.vertical ? "margin-top" : "margin-left"}`]: "1rem"';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, true);
+  });
+
+  test("Should return false when no props are present in styled component", function () {
+    const code =
+      'const VariableExample = styled.div`\n  background-color: ${someVar};\n  ${someVar && "flex-direction: column;"};\n  ${anotherVar ? "margin-top" : "margin-left"}: 1rem;\n`;';
+    const match = containsProps(code);
+
+    assert.strictEqual(match, false);
   });
 });
 
